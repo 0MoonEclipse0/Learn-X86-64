9a873169d56a18dc6e2740b4266f9cff102e38cd
0MoonEclipse0 <ben@snorc.org>
2024-07-05 18:18:57 +0200

    Delete main directory

--- main/scripts.js
+++
@@ -1,9 +0,0 @@
-function copyToClipboard(id) {
-  const codeElement = document.getElementById(id);
-  const range = document.createRange();
-  range.selectNode(codeElement);
-  window.getSelection().removeAllRanges(); // Clear previous selections
-  window.getSelection().addRange(range); // Select the text
-  document.execCommand("copy"); // Copy the selected text
-  window.getSelection().removeAllRanges(); // Deselect
-}
